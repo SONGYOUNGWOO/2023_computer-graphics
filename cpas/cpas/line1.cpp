0048af934cc0b97ae7331f42094b90d9f520060f
syw(desktop) <syw9846@naver.com>
2023-09-08 08:50:34 +0900

    문제2 0908

--- cpas/cpas/line1.cpp
+++ cpas/cpas/line1.cpp
@@ -12,50 +12,22 @@ uniform_int_distribution<int> uid(0, 2); //랜덤값범위
 
 // cout printf
 // det 행렬식 값 
-void det_three (int x[3][3]) {
+int det_three (int x[3][3]) {
 	int dd= x[0][0] * (x[1][1] * x[2][2] - x[2][1] * x[1][2])
 
 		- x[0][1] * (x[1][0] * x[2][2] - x[1][2] * x[2][0])
 
 		+ x[0][2] * (x[1][0] * x[2][1] - x[1][1] * x[2][0]);
-	cout << dd << endl;
+	//cout << dd << endl;
+	return dd;
 }
-// 가로출력
-void det_our(int x[3][3],int y[3][3],int r[3][3]) {
-	for (int a = 0; a < 3; a++)
-	{
-		cout << "|";
-		for (int b = 0; b < 3; b++) {
-			cout << x[a][b];
-		}cout << endl;
-		cout << "|";
-		cout << " ";	//공백
-		cout << "|";
-		for (int b = 0; b < 3; b++) {
-			cout << y[a][b];
-		}cout << endl;
-		cout << "|";
-		cout << " ";	//공백
-		cout << "|";
-		for (int b = 0; b < 3; b++) {
-			cout << r[a][b];
-		}cout << endl;
-		cout << "|";
 
+int det_four(int A[4][4],int x[3][3]) {
+	int dd = det_three(x);
+	int ddd = dd * 1; // 여인수를 만들때 소행렬식을 사용하여 한 행이 0001이기에 가능.[Aij] = (-1)^i+j[Mij] 
+	return ddd;
+}
 
-	}cout << endl;
-}
-void det_four(int A[4][4]) {
-	int ddd = (A[0][0] * A[1][1] * A[2][2] * A[3][3]) - (A[0][0] * A[1][1] * A[2][3] * A[3][2]) +(A[0][0] * A[1][2] * A[2][1] * A[3][3])
-			+ (A[0][0] * A[1][2] * A[2][3] * A[3][1]) + (A[0][0] * A[1][3] * A[2][1] * A[3][2]) - (A[0][0] * A[1][3] * A[2][2] * A[3][1])
-			- (A[0][1] * A[1][0] * A[2][2] * A[3][3]) + (A[0][1] * A[1][0] * A[2][3] * A[3][2]) + (A[0][1] * A[1][2] * A[2][0] * A[3][3])
-			- (A[0][1] * A[1][2] * A[2][3] * A[3][0]) - (A[0][1] * A[1][3] * A[2][0] * A[3][2]) + (A[0][1] * A[1][3] * A[2][2] * A[3][0])
-			+ (A[0][2] * A[1][0] * A[2][1] * A[3][3]) - (A[0][2] * A[1][0] * A[2][3] * A[3][1]) - (A[0][2] * A[1][1] * A[2][0] * A[3][3])
-			+ (A[0][2] * A[1][1] * A[2][3] * A[3][0]) + (A[0][2] * A[1][3] * A[2][0] * A[3][1]) - (A[0][2] * A[1][3] * A[2][1] * A[3][0])
-			- (A[0][3] * A[1][0] * A[2][1] * A[3][2]) + (A[0][3] * A[1][0] * A[2][2] * A[3][1]) + (A[0][3] * A[1][1] * A[2][0] * A[3][2])
-			- (A[0][3] * A[1][1] * A[2][2] * A[3][0]) - (A[0][3] * A[1][2] * A[2][0] * A[3][1]) + (A[0][3] * A[1][2] * A[2][1] * A[3][0]);
-	cout << ddd << endl;
-}
 
 void rand(int x[3][3]) {
 	for (int i = 0; i < 3; i++) {
@@ -94,8 +66,8 @@ void H(int x[3][3]) {
 		cout << endl;
 	}
 
-	det_four(th);
-
+	det_four(th,x);
+	cout << det_four(th, x) << endl << endl;
 }
 
 int main() {
@@ -110,6 +82,8 @@ int main() {
 	while (true) {
 
 		cin >> ta;
+		cout << endl;
+
 		int ee = ta[0] - '0';
 		// cin >> num;
 		//문자인지 숫자인지 구별
@@ -170,15 +144,19 @@ int main() {
 			//r: 행렬식의 값 (Determinant) void det 
 			if (strcmp(ta, "r") == 0) {
 				det_three(x);
+				cout << det_three(x) << endl << endl;
 				det_three(y);
+				cout << det_three(y) << endl << endl;
 				continue;
 			}
 			//t: 전치 행렬(Transposed matrix)과 그 행렬식의 값을 입력한 2개의 행렬에 모두 적용한다.
 			if (strcmp(ta, "t") == 0) {
 				Transposed(x);
 				det_three(x);
+				cout << det_three(x) << endl << endl;
 				Transposed(y);
 				det_three(y);
+				cout << det_three(y) << endl << endl;
 				continue;
 			}
 			//h: 3X3 행렬을 4X4 행렬로 변환하고 행렬식의 값 (4행4열 행렬식 값) 출력
@@ -221,3 +199,38 @@ int main() {
 	}
 
 }
+/*void det_our(int x[3][3], int y[3][3], int r[3][3]) {
+	for (int a = 0; a < 3; a++)
+	{
+		cout << "|";
+		for (int b = 0; b < 3; b++) {
+			cout << x[a][b];
+		}cout << endl;
+		cout << "|";
+		cout << " ";	//공백
+		cout << "|";
+		for (int b = 0; b < 3; b++) {
+			cout << y[a][b];
+		}cout << endl;
+		cout << "|";
+		cout << " ";	//공백
+		cout << "|";
+		for (int b = 0; b < 3; b++) {
+			cout << r[a][b];
+		}cout << endl;
+		cout << "|";
+
+
+	}cout << endl;
+}
+/*int det_three (int x[3][3]) {
+* det =			  (A[0][0] * A[1][1] * A[2][2] * A[3][3]) - (A[0][0] * A[1][1] * A[2][3] * A[3][2]) + (A[0][0] * A[1][2] * A[2][1] * A[3][3])
+				+ (A[0][0] * A[1][2] * A[2][3] * A[3][1]) + (A[0][0] * A[1][3] * A[2][1] * A[3][2]) - (A[0][0] * A[1][3] * A[2][2] * A[3][1])
+				- (A[0][1] * A[1][0] * A[2][2] * A[3][3]) + (A[0][1] * A[1][0] * A[2][3] * A[3][2]) + (A[0][1] * A[1][2] * A[2][0] * A[3][3])
+				- (A[0][1] * A[1][2] * A[2][3] * A[3][0]) - (A[0][1] * A[1][3] * A[2][0] * A[3][2]) + (A[0][1] * A[1][3] * A[2][2] * A[3][0])
+				+ (A[0][2] * A[1][0] * A[2][1] * A[3][3]) - (A[0][2] * A[1][0] * A[2][3] * A[3][1]) - (A[0][2] * A[1][1] * A[2][0] * A[3][3])
+				+ (A[0][2] * A[1][1] * A[2][3] * A[3][0]) + (A[0][2] * A[1][3] * A[2][0] * A[3][1]) - (A[0][2] * A[1][3] * A[2][1] * A[3][0])
+				- (A[0][3] * A[1][0] * A[2][1] * A[3][2]) + (A[0][3] * A[1][0] * A[2][2] * A[3][1]) + (A[0][3] * A[1][1] * A[2][0] * A[3][2])
+				- (A[0][3] * A[1][1] * A[2][2] * A[3][0]) - (A[0][3] * A[1][2] * A[2][0] * A[3][1]) + (A[0][3] * A[1][2] * A[2][1] * A[3][0]);
+* }
+*/
--- cpas/cpas/line2.cpp
+++ cpas/cpas/line2.cpp
@@ -1,19 +1,102 @@
 #include <iostream>
 #include <fstream> // 파일읽는 헤더파일 ifstream 사용가능
-#include <string> 
+#include <string> //getline 함수 사용
+
 using namespace std;
 
-int main() { 
-	string line;
+int main() {
+    ifstream file{ "data.txt" };   // 생성과 동시에 연다.
 
-	ifstream file("data.txt");   //열다
-	
-	if (file.is_open()) {
-		while (getline(file, line)) {
-			cout << line << endl;
-		}
+    char c;
+    file >> noskipws;               //공백 무시 x, 메모장 그대로
+    int word_count = 0;
+    int number_count = 0;
+    int upper_count = 0;
 
-	}
-	
-	
+    //한 줄씩 읽기 , etline 함수는 파일에서 개행문자 (\n) 이 나올 때 가지 최대 지정한 크기 - 1 만큼 읽게됩니다
+    // EOF라면 입력이 취소되고 cin.eof()는 true를 반환한다. 
+    // 이를 이용하여 파일이 종료될 때까지 입력을 받는 코드를 작성할 수 있다.
+
+    int number;
+    bool num = false; // 맞다 아니다
+
+    std::string str;
+
+    // 파일 전체를 읽어 문자열에 저장합니다.
+    file.seekg(0, ios::beg);
+    file.read(&str[0], str.capacity());
+  
+
+    while (file >> c) {
+        cout << c;
+        
+ 
+
+
+        // 공백임을 확인 , \n
+        if (isspace(c)) {              //뒤가 숫자였으면
+            if (num == true) {
+                number_count++;
+                num = false;
+                
+            }
+            //                        //뒤가 문자였으면
+            else if (num == false) {
+                word_count++;
+                num = true;
+            }
+        }
+        // 숫자
+        else if(isdigit(c)){
+            if (num == true) {}     // 숫자 뒤 숫자
+            else 
+            {
+                num = true;         // 공백 뒤 숫자
+            }
+        }
+        // 문자
+        else if (isalpha(c)) {
+          
+            if (num == false) {}    // 문자 뒤 문자
+            else if (num == true) {  // 공백 뒤 문자
+                num = false;
+            }
+            else {
+                num = false;        // 숫자 뒤 문자
+            }
+        }
+        else if (isupper(c)) {
+            upper_count++;
+        }
+        
+        //특수문자 체크
+        else {
+            num = false;
+        }
+         
+
+       
+    }
+
+    cout << endl << endl;
+    
+    cout << "word count:: " << word_count << endl;
+    cout << "number count: " << number_count << endl;
+    cout << "Capital word: " << upper_count << endl;
+
 }
+
+       
+
+
+
+
+	
+	
+
+
+
+   
+   
+
+
